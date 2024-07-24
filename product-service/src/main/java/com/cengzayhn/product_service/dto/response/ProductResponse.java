35eec90854e8246bbaba1d5b35953d325f63e53d
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-07-24 23:37:33 +0300

    newly updated structure

---
+++ product-service/src/main/java/com/cengzayhn/product_service/dto/response/ProductResponse.java
@@ -0,0 +1,19 @@
+package com.cengzayhn.product_service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
---
+++ product-service/src/main/java/com/cengzayhn/product_service/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.cengzayhn.product_service.repository;
+
+import com.cengzayhn.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
