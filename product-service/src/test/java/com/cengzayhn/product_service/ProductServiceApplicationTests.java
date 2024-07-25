ba9c363a363e29eaa9e55c2f259780067c3e45fb
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-07-25 23:15:37 +0300

    getAllProducts test added into ProductServiceApplicationTests.java

--- product-service/src/test/java/com/cengzayhn/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/cengzayhn/product_service/ProductServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cengzayhn.product_service;
 
+import com.cengzayhn.product_service.dto.response.ProductResponse;
+import com.cengzayhn.product_service.model.Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.cengzayhn.product_service.dto.request.ProductRequest;
 import com.cengzayhn.product_service.repository.ProductRepository;
@@ -17,6 +19,7 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -61,4 +64,39 @@ class ProductServiceApplicationTests {
 		Assertions.assertEquals(1, productRepository.findAll().size());
 	}
 
+
+	@Test
+	void shouldGetAllProducts() throws Exception{
+		//Mock products to fill database
+		Product mockProduct1 = new Product();
+		mockProduct1.setName("Product One");
+		mockProduct1.setDescription("Product One Description");
+		mockProduct1.setPrice(BigDecimal.valueOf(1300));
+		productRepository.save(mockProduct1);
+
+		Product mockProduct2 = new Product();
+		mockProduct2.setName("Product Two");
+		mockProduct2.setDescription("Product Two Description");
+		mockProduct2.setPrice(BigDecimal.valueOf(1400));
+		productRepository.save(mockProduct2);
+
+		String responseString = mockMvc.perform(MockMvcRequestBuilders.get("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+		)
+				.andExpect(status().isOk())
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+		List<ProductResponse> productResponses = objectMapper
+				.readValue(responseString,objectMapper.getTypeFactory()
+				.constructCollectionType(List.class,ProductResponse.class));
+
+		Assertions.assertEquals(2, productResponses.size());
+		Assertions.assertEquals(mockProduct1.getName(), productResponses.get(0).getName());
+		Assertions.assertEquals(mockProduct1.getDescription(), productResponses.get(0).getDescription());
+		Assertions.assertEquals(mockProduct1.getPrice(), productResponses.get(0).getPrice());
+		Assertions.assertEquals(mockProduct2.getName(), productResponses.get(1).getName());
+		Assertions.assertEquals(mockProduct2.getDescription(), productResponses.get(1).getDescription());
+		Assertions.assertEquals(mockProduct2.getPrice(), productResponses.get(1).getPrice());
+	}
 }
