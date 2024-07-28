db49765f1065c1aa249b93c3bea9b1dd1bf2a4d3
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-07-28 22:56:21 +0300

    OrderService has been created and placeOrder method implemented

---
+++ order-service/src/main/java/com/cengzayhn/order_service/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.cengzayhn.order_service.service;
+
+import com.cengzayhn.order_service.dto.OrderLineItemsDTO;
+import com.cengzayhn.order_service.dto.request.OrderRequest;
+import com.cengzayhn.order_service.model.Order;
+import com.cengzayhn.order_service.model.OrderLineItems;
+import com.cengzayhn.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest
+                .getOrderLineItemsDTOList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDTO orderLineItemsDTO) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDTO.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDTO.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDTO.getSkuCode());
+        return orderLineItems;
+    }
+}
