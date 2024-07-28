edf8ad177c0ad161a28eafc58a3d8a3956c76ae1
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-07-28 22:55:17 +0300

    OrderRepository has been created

---
+++ order-service/src/main/java/com/cengzayhn/order_service/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.cengzayhn.order_service.repository;
+
+import com.cengzayhn.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
