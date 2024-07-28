f0cf0eea8dfb981ca82672898bc4218994758407
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-07-28 22:55:33 +0300

    OrderRequest dto has been created

---
+++ order-service/src/main/java/com/cengzayhn/order_service/dto/request/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.cengzayhn.order_service.dto.request;
+
+import com.cengzayhn.order_service.dto.OrderLineItemsDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDTO> orderLineItemsDTOList;
+}
