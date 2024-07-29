b32fca9fe69724ab7ce2b9bcd6bbc67f6b618fcb
Mehmet Cengiz Ayhan <mcengzayhn@gmail.com>
2024-07-29 23:03:32 +0300

    loaddata bean added in order to fill database while migrating the inventory-service

--- inventory-service/src/main/java/com/cengzayhn/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/cengzayhn/inventory_service/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.cengzayhn.inventory_service;
 
+import com.cengzayhn.inventory_service.model.Inventory;
+import com.cengzayhn.inventory_service.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("inventory 1");
+			inventory1.setQuantity(100);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("inventory 2");
+			inventory2.setQuantity(0);
+
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+		};
+	}
 }
